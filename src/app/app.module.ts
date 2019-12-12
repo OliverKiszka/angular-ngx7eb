1adddaaaebfb8aebe21a7b3276587d64b8cef495
damiansierocki <damiansierocki@outlook.com>
2019-12-12 16:18:42 +0100

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ListaWydatkowComponent } from './lista-wydatkow/lista-wydatkow.component';
+import { WydatkiService } from './wydatki.service';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, ListaWydatkowComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [WydatkiService]
 })
 export class AppModule { }
