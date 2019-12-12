8c6c0e3868203ab338dc4a6bb20165dbdc8c03d3
damiansierocki <damiansierocki@outlook.com>
2019-12-12 15:52:28 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,9 @@ ngOnInit() {
         new Wydatek('Myjnia', 75, '', new Date(2018, 4, 2)),
         new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
         new Wydatek('Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
-        new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12), 10, 100),
-        new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 5, 12), 15, null),
-        new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 11),null, 20)
+        new Wydatek('Tankowanie', 300, 'Litry oraz kilometry', new Date(2017, 7, 12), 300, 20),
+        new Wydatek('Tankowanie', 338.46, 'Tylko kilometry, "null" litrów ', new Date(2017, 5, 10), 15, null),
+        new Wydatek('Tankowanie', 276.34, 'Tylko litry, "null" kilometrów', new Date(2017, 8, 30),null, 30)
     ];
 }
 
