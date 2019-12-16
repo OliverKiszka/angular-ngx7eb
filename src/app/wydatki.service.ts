403d4ebd65f702978a46b68ad1af198df7c281ec
damiansierocki <damiansierocki@outlook.com>
2019-12-16 17:30:23 +0100

    1 file modified

--- src/app/wydatki.service.ts
+++ src/app/wydatki.service.ts
@@ -15,7 +15,7 @@ export class WydatkiService {
       new Wydatek(3, 'Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
       new Wydatek(4, 'Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16),55,55),
       new Wydatek(5, 'Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12), 527),
-      new Wydatek(6, 'Tankowanie', 257.30, '', new Date(2017, 7, 13), null, null)
+      new Wydatek(6, 'Tankowanie', 257.30, '', new Date(2017, 7, 13), null, 35)
     ];
     this.nextId = 7;
   }
